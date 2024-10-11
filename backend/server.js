677d6eba462bdaa71ef0566101d761b5c7b93027
mofey <mofey@outlook.com>
2024-10-11 23:20:57 +0100

    day5

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,30 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import connectDB from './config/db.js';
-import productRoutes from './routes/product.routes.js';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// Connect to the database
-connectDB();
+const __dirname = path.resolve();
 
-// Middleware to parse JSON data in the req.body
-app.use(express.json());
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-// API routes
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve static files from the frontend build directory
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-    app.get('*', (req, res) => {
-        // Render the index.html file as our React application
-        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-    });
-} else {
-    console.log('Not in production mode');
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
